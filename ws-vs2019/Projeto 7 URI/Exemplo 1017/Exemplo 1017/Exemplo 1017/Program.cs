b58ba5705c787e976b77782c1069566ea5bc61f0
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2020-11-11 23:36:28 -0300

    Exercicio de fixação simples

---
+++ ws-vs2019/Projeto 7 URI/Exemplo 1017/Exemplo 1017/Exemplo 1017/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exemplo_1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo, velocidade;
+            double  litros, distancia;
+
+            Console.WriteLine("Digite o tempo gasto na viagem: ");
+            tempo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a velocidade media: ");
+            velocidade = int.Parse(Console.ReadLine());
+
+            distancia = tempo * velocidade;
+            litros = distancia / 12.0;
+
+            Console.WriteLine("A quantidade de litros necessarios para essa viagem são : " + litros.ToString("F3", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+            
+        }
+    }
+}
