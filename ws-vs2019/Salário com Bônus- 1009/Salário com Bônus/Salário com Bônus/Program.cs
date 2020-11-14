88505a302273263846cf913122018783fc2150f5
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2020-11-14 00:48:58 -0300

    Up de um novo exercecio de porcentagem

---
+++ ws-vs2019/Salário com Bônus- 1009/Salário com Bônus/Salário com Bônus/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Salário_com_Bônus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Salário com Bônus
+
+            //declaração de variaveis
+            string nome;
+            double salario_fixo, vendas, total;
+
+            Console.WriteLine("Nome do vendedor: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Salario : ");
+            salario_fixo = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Informe o valor total de vendas: ");
+            vendas = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            total = vendas * 15.0 / 100 + salario_fixo;
+            
+
+            Console.WriteLine("O salario total de " + nome + " é de : " + "TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+
+        }
+    }
+}
