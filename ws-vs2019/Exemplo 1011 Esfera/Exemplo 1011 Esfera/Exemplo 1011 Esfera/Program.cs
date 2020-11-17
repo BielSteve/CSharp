306d67a9857bf0fa50162eebffed56cb3b9b3abd
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2020-11-16 23:11:42 -0300

    Exercio com raiz cubica

---
+++ ws-vs2019/Exemplo 1011 Esfera/Exemplo 1011 Esfera/Exemplo 1011 Esfera/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exemplo_1011_Esfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exemplo 1011 Esfera
+
+            //Declaração de variaveis
+            double r, v, pi = 3.14159;
+
+            Console.WriteLine("Digite o valor do raio : ");
+            r = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            //Fazendo calculo da raiz cubica
+            v = (4 / 3.0) * pi * Math.Pow(r, 3.0);
+
+            Console.WriteLine("O VOLUME É = " + v.ToString("F3",CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+        }
+    }
+}
