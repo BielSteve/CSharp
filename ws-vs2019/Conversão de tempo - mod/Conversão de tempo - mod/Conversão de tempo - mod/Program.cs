4019ef55577532987401180ff4f4cb7cdf60781f
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2020-11-22 14:50:04 -0300

    Exercicio utilizando o mod

---
+++ ws-vs2019/Conversão de tempo - mod/Conversão de tempo - mod/Conversão de tempo - mod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Conversão_de_tempo___mod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conversão de tempo - mod
+
+            //Declaração de variaveis
+            int N, horas, resto, minutos, segundos;
+
+            Console.WriteLine("Digite o numero para conversão: ");
+            N = int.Parse(Console.ReadLine());
+
+            horas = N / 3600;
+            resto = N % 3600;
+
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine("Resultado = " + horas + ":" + minutos + ":" + segundos);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ws-vs2019/Idade em Dias - mod 1020/Idade em Dias - mod 1020/Idade em Dias - mod 1020/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Idade_em_Dias___mod_1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Idade em Dias - mod 1020
+
+            //Declaração de variaveis
+            int valor, anos, resto, meses, dias;
+
+            Console.WriteLine("Digite o valor em dias para conversão: ");
+            valor = int.Parse(Console.ReadLine());
+
+            anos = valor / 365;
+            resto = valor % 365;
+            meses = resto / 30;
+            dias = resto % 30;
+
+            Console.WriteLine(anos + " ano(s)");
+            Console.WriteLine(meses + " mes(es)");
+            Console.WriteLine(dias + " dia(s)");
+
+            Console.ReadLine();
+
+        }
+    }
+}
