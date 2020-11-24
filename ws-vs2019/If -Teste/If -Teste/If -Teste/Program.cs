927def9578c91ee89e461d29b2a2a8efcb8722cf
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2020-11-23 22:55:13 -0300

    Teste simples com If

---
+++ ws-vs2019/If -Teste/If -Teste/If -Teste/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace If__Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaração de variaveis
+
+            int horas;
+
+            Console.WriteLine("Que horas são ?");
+            horas = int.Parse(Console.ReadLine());
+
+
+            if (horas <= 12)
+            {
+                Console.WriteLine("Bom dia");
+            }
+            else
+            {
+                if (horas < 18)
+                {
+                    Console.WriteLine("Boa Tarde");
+                }
+                else
+                {
+                    Console.WriteLine("Boa noite"); 
+                }
+            }
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
