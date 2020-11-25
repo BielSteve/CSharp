657c0685e576821b2e0f6b990eaeceb7ea2131a3
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2020-11-24 23:14:04 -0300

    Formula de Bhaskara - IF

---
+++ ws-vs2019/Fórmula de Bhaskara- 1036 - IF/Fórmula de Bhaskara- 1036 - IF/Fórmula de Bhaskara- 1036 - IF/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Fórmula_de_Bhaskara__1036___IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fórmula de Bhaskara- 1036 - IF
+
+            //Declaração de variaveis
+            double a, b, c, delta, r1, r2;
+
+            Console.WriteLine("Digite o valor de A, B e C : ");
+            String[] vet = Console.ReadLine().Split(' ');
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+            if (a == 0 || delta < 0.0)
+            {
+                Console.WriteLine("Impossivel Calcular");
+            }
+            else
+            {
+                r1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+                r2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+                Console.WriteLine("R1 = " + r1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + r2.ToString("F5", CultureInfo.InvariantCulture));
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ws-vs2019/Nota Escolar - If/Nota Escolar - If/Nota Escolar - If/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Nota_Escolar___If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nota Escolar - If
+
+            //Declaração de Variaveis
+            double nota1, nota2, soma;
+
+            Console.WriteLine("Digite a nota 1 :");
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite a nota 2 :");
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            soma = nota1 + nota2;
+
+            Console.WriteLine("NOTA FINAL = " + soma.ToString("F1"),CultureInfo.InvariantCulture);
+
+            if (soma < 60)
+            {
+                Console.WriteLine("REPROVADO OTARIO!");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
