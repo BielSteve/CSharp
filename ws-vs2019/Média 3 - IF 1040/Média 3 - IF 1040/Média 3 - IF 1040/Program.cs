526f76b84e3c6c85e4104332e716db03c1911501
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2020-12-04 22:26:28 -0300

    Exercicios com IF, nivel maior de dificuldade

---
+++ ws-vs2019/Média 3 - IF 1040/Média 3 - IF 1040/Média 3 - IF 1040/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+
+namespace Média_3___IF_1040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Média 3 - IF 1040
+
+            //declaração de variaveis
+
+            float N1, N2, N3, N4, exame, media, media_final;
+
+            Console.WriteLine("Digite as notas do alunos em sequencia : ");
+            String[] vet = Console.ReadLine().Split(' ');
+            N1 = float.Parse(vet[0], CultureInfo.InvariantCulture);
+            N2 = float.Parse(vet[1], CultureInfo.InvariantCulture);
+            N3 = float.Parse(vet[2], CultureInfo.InvariantCulture);
+            N4 = float.Parse(vet[3], CultureInfo.InvariantCulture);
+            
+
+            media = (N1*2 + N2*3 + N3*4 + N4*1 ) / (2+3+4+1);
+
+            if (media >= 7.0)
+            {
+                Console.WriteLine("Média: " + media.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Aluno Aprovado");
+            }
+            else if (media < 5.0)
+            {
+                Console.WriteLine("Média: " + media.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Aluno Reprovado");
+            }
+            else if (media >= 5.0 && media <= 6.9)
+            {
+                Console.WriteLine("Média: " + media.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Aluno em exame");
+
+                Console.WriteLine("Digite a nota do exame: ");
+                exame = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                media_final = (media + exame) / 2;
+
+                if (media_final >= 5.0)
+                {
+                    Console.WriteLine("Média: " + media.ToString("F1", CultureInfo.InvariantCulture));
+                    Console.WriteLine("Aluno em exame");
+                    Console.WriteLine("Nota do exame: " + exame.ToString("F1", CultureInfo.InvariantCulture));
+                    Console.WriteLine("Aluno Aprovado!");
+                    Console.WriteLine("Média Final: " + media_final.ToString("F1", CultureInfo.InvariantCulture));
+                }
+                else if (media_final <= 4.9)
+                {
+                    Console.WriteLine("Média: " + media.ToString("F1", CultureInfo.InvariantCulture));
+                    Console.WriteLine("Aluno em exame");
+                    Console.WriteLine("Nota do exame: " + exame.ToString("F1", CultureInfo.InvariantCulture));
+                    Console.WriteLine("Aluno Reprovado!");
+                    Console.WriteLine("Média Final: " + media_final.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+
+           
+
+
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ ws-vs2019/Tipos de Triângulos - IF 1045/Tipos de Triângulos - IF 1045/Tipos de Triângulos - IF 1045/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Globalization;
+
+namespace Tipos_de_Triângulos___IF_1045
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipos de Triângulos - IF 1045
+
+            //declaração de variaveis
+
+            float n1, n2, n3, A, B, C;
+
+            Console.WriteLine("Digite 3 valores para verificarmos as possibilidades de triângulos: ");
+            String[] vet = Console.ReadLine().Split(' ');
+            n1 = float.Parse(vet[0], CultureInfo.InvariantCulture);
+            n2 = float.Parse(vet[1], CultureInfo.InvariantCulture);
+            n3 = float.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            if (n1 > n2 && n1 > n3)
+            {
+                A = n1;
+                if (n2 > n3)
+                {
+                    B = n2;
+                    C = n3;
+                }
+                else
+                {
+                    B = n3;
+                    C = n2;
+                }
+            }
+            else if (n2 > n3)
+            {
+                A = n2;
+                if (n1 > n3)
+                {
+                    B = n1;
+                    C = n3;
+                }
+                else
+                {
+                    B = n3;
+                    C = n1;
+                }
+            }
+            else
+            {
+                A = n3;
+                if (n1 > n2)
+                {
+                    B = n1;
+                    C = n2;
+                }
+                else
+                {
+                    B = n2;
+                    C = n1;
+                }
+            }
+
+            if (A >= B + C)
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO");
+            }
+            else
+            {
+                // primeiro vamos testas as tres possibilidades em relacao a angulos
+                if (A * A == B * B + C * C)
+                {
+                    Console.WriteLine("TRIANGULO RETANGULO");
+                }
+                else if (A * A > B * B + C * C)
+                {
+                    Console.WriteLine("TRIANGULO OBTUSANGULO");
+                }
+                else
+                {
+                    Console.WriteLine("TRIANGULO ACUTANGULO");
+                }
+
+                // agora vamos testar se pode ser equilatero ou isosceles
+                if (A == B && B == C)
+                {
+                    Console.WriteLine("TRIANGULO EQUILATERO");
+                }
+                else if (A == B || A == C || B == C)
+                {
+                    Console.WriteLine("TRIANGULO ISOSCELES");
+                }
+            }
+
+        }
+    }
+}
