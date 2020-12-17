3a75a9d79d80873beddb492e3987b3a1e77c09f1
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2020-12-16 23:37:48 -0300

    Exercicios com While

---
+++ ws-vs2019/Idades - While 1154/Idades - While 1154/Idades - While 1154/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Idades___While_1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Idades - While 1154
+
+
+            //Declaração de variaveis
+            int idade, cont = 0;
+            double media, soma = 0.0;
+
+            Console.WriteLine("Digite sua idade otario: ");
+            idade = int.Parse(Console.ReadLine());
+
+            while (idade >= 0)
+            {
+                cont++;
+                soma = soma + idade;
+                idade = int.Parse(Console.ReadLine());
+
+            }
+
+            media = soma / cont;
+
+            Console.WriteLine("A média das idades é : " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
---
+++ ws-vs2019/Tipo de Combustível - While 1134/Tipo de Combustível - While 1134/Tipo de Combustível - While 1134/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Tipo_de_Combustível___While_1134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipo de Combustível - While 1134
+
+            //Declaração de variaveis
+            int tipo, cont1=0, cont2=0, cont3=0;
+
+            Console.WriteLine("Digite o tipo de combustivel : ");
+            tipo = int.Parse(Console.ReadLine());
+
+            while (tipo != 4)
+            {
+                if (tipo == 1)
+                {
+                    cont1++;
+                }
+                else if (tipo == 2)
+                {
+                    cont2++;
+                }
+                else if (tipo == 3)
+                {
+                    cont3++;
+                }
+                else
+                {
+                    Console.WriteLine("Digite o codido de 1 - 4 apenas otario");
+                }
+                tipo = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool : " + cont1);
+            Console.WriteLine("Gasolina : " + cont2);
+            Console.WriteLine("Diesel : " + cont3);
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
