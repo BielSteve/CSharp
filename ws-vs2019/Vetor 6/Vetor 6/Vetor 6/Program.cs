54f86efe1929ecc3e1ebff2d8f88bfbd58bbda2f
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2021-01-19 23:35:26 -0300

    Exercicios com vetor

---
+++ ws-vs2019/Vetor 6/Vetor 6/Vetor 6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Vetor_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vetor 6
+            //Biel Steve
+
+            int n, idade_velho=0;
+            string nome_velho = "nulo";
+
+            Console.WriteLine("Digite quantas repetições : ");
+            n = int.Parse(Console.ReadLine());
+
+            string[] nomes = new string[n];
+            int[] idades = new int[n];
+
+            Console.WriteLine("Digite nome e idade : ");
+
+            for (int i = 0; i<n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                nomes[i] = s[0];
+                idades[i] = int.Parse(s[1]);
+            }
+
+            for (int i=0; i<n; i++)
+            {
+                if (idades[i] > idade_velho)
+                {
+                    idade_velho = idades[i];
+                    nome_velho = nomes[i];
+                }
+            }
+
+            Console.WriteLine("Pessoa mais velha : " + nome_velho);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ws-vs2019/Várias Notas Com Validação - 1118 - While/Várias Notas Com Validação - 1118 - While/Várias Notas Com Validação - 1118 - While/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Várias_Notas_Com_Validação___1118___While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
