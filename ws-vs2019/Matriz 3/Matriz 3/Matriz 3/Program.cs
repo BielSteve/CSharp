70540b7e3bd324a03e3a2ab1e1f68f8deaabf79f
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2021-01-25 23:39:14 -0300

    Exercicios com matriz

---
+++ ws-vs2019/Matriz 3/Matriz 3/Matriz 3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Matriz_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Matriz 3
+            //Biel Steve
+
+            int n;
+
+            Console.WriteLine("Digite quantidade de linhas e colunas: ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            Console.WriteLine("Digite os valores: ");
+
+            for (int i=0; i<n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j=0; j<n; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            Console.WriteLine("Maiores de cada linha : ");
+            
+            for (int i = 0; i < n; i++)
+            {
+                int maior = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i,j] > maior)
+                    {
+                        maior = mat[i, j];
+                    }
+                }
+                Console.WriteLine(maior);
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ ws-vs2019/Matriz 4/Matriz 4/Matriz 4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Matriz_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Matriz 4
+            //Biel Steve
+
+            int n;
+
+            Console.WriteLine("Digite a quantidade de linhas e colunas: ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            Console.WriteLine("Digite os valores: ");
+            for (int i=0; i<n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j=0; j<n; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            Console.WriteLine("Resultado: ");
+            int soma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                { // j inicia com i+1 para que ele possa ser incrementado a cada repetição e pegue o proximo valor apos a diagonal
+                    soma = soma + mat[i, j];
+                }
+            }
+
+
+            Console.WriteLine(soma);
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
