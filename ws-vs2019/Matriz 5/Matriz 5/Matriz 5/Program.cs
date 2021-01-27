a07442f5cd692d209ffa844078da80dd9ee71a5b
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2021-01-27 00:00:02 -0300

    Exercicios com matriz

---
+++ ws-vs2019/Matriz 5/Matriz 5/Matriz 5/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Matriz_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Matriz 5
+            //Biel Steve
+
+            int n, m;
+
+            Console.WriteLine("Digite quantas linhas e colunas: ");
+            string[] s = Console.ReadLine().Split(' ');
+            n = int.Parse(s[0]);
+            m = int.Parse(s[1]);
+
+            int[,] a = new int[n, m];
+            int[,] b = new int[n, m];
+            int[,] c = new int[n, m];
+
+
+            Console.WriteLine("Digite os valores A: ");
+            for (int i=0; i<n; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+                for (int j=0; j<m; j++)
+                {
+                    a[i, j] = int.Parse(s[j]);
+                }
+            }
+            Console.WriteLine("Digite os valores B: ");
+            for (int i = 0; i < n; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < m; j++)
+                {
+                    b[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            Console.WriteLine("Valores da Matriz C : ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    c[i, j] = a[i, j] + b[i, j];
+                    Console.Write(c[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ ws-vs2019/Matriz 6/Matriz 6/Matriz 6/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Globalization;
+
+namespace Matriz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Matriz 6
+            //Biel Steve
+
+            int n;
+
+            Console.WriteLine("Digite a quantidade linhas e colunas: ");
+            n = int.Parse(Console.ReadLine());
+
+            double[,] mat = new double[n, n];
+
+            Console.WriteLine("Digite os valores : ");
+            for (int i=0; i<n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j=0; j<n; j++)
+                {
+                    mat[i, j] = double.Parse(s[j], CultureInfo.InvariantCulture);
+                }
+            }
+
+            
+            double soma = 0.0; ;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i,j] > 0)
+                    {
+                        soma = soma + mat[i, j];
+                    }
+                }
+            }
+
+            int linha;
+            Console.WriteLine("Escolha a linha: ");
+            linha = int.Parse(Console.ReadLine());
+            int coluna;
+            Console.WriteLine("Escolha a coluna: ");
+            coluna = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Resultados: ");
+            Console.WriteLine("SOMA DOS POSITIVOS: " + soma.ToString("F1", CultureInfo.InvariantCulture));
+            Console.Write("LINHA ESCOLHIDA: ");
+            for (int j = 0; j < n; j++)
+            {
+                    Console.Write(mat[linha, j].ToString("F1", CultureInfo.InvariantCulture) + " ");
+
+            }
+            Console.WriteLine();
+            Console.Write("COLUNA ESCOLHIDA: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = coluna; j < n; j++)
+                {
+                    Console.Write(mat[i, j].ToString("F1", CultureInfo.InvariantCulture) + " ");
+                }
+            }
+            Console.WriteLine();
+
+            Console.Write("DIAGONAL PRINCIPAL : ");
+            for (int i = 0; i < n; i++)
+            {
+                
+                    Console.Write(mat[i, i].ToString("F1", CultureInfo.InvariantCulture) + " ");
+            }
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i,j] < 0.0)
+                    {
+                        mat[i, j] = mat[i, j] * mat[i, j];
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("MATRIZ ALTERADA: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(mat[i, j].ToString("F1", CultureInfo.InvariantCulture) + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
